b4baf0a02d7504fd95316547a7227b15c0144e8f
Paul Edwards <mail@edwardsp.co.uk>
2013-03-19 16:56:10 +0000

    Updated to include total and headers

--- src/mock_database.py
+++ src/mock_database.py
@@ -9,12 +9,14 @@ class MockDatabase:
 	{ 'Number of authors' : { 'Conference Paper':10, 'Journal':5, 'Book':8, 'Book Chapter':2 } }
 ]
 
+	# Return tuple containing headers and list of data
         def get_publications_by_author(self):
-                return [ ('Author1', 1, 2, 3, 4), ('Author2', 5, 6, 7, 8) ]
-
+                return (('Author', 'Number of conference papers', 'Number of journals', 'Number of books', 'Number of book chapters', 'Total'), [ ('Author1', 1, 2, 3, 4, 10), ('Author2', 5, 6, 7, 8, 26) ])
+	# Return tuple containing headers and list of data
         def get_publications_by_year(self):
-                return [ (2002, 100, 50, 25, 10), (2004, 99, 49, 24, 9) ]
+                return (('Author', 'Number of conference papers', 'Number of journals', 'Number of books', 'Number of book chapters'), [ (2002, 100, 50, 25, 10), (2004, 99, 49, 24, 9) ])
 
+	# Return tuple containing headers and list of data
         def get_author_totals_by_year(self):
-                return [ (2001, 10, 5, 6, 3), (2003, 12, 7, 4, 2) ]
+                return (('Author', 'Number of conference papers', 'Number of journals', 'Number of books', 'Number of book chapters'), [ (2001, 10, 5, 6, 3), (2003, 12, 7, 4, 2) ])
 
