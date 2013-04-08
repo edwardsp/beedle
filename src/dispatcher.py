b87234c8d070028adfe0f2ed3324a504b5dad4ce
Paul Edwards <mail@edwardsp.co.uk>
2013-04-08 22:31:52 +0100

    All js added to static, icon, tiding

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 import mock_database
 import database
+import os
 import sys
 app = Flask(__name__)
 
 db = None
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(
+		os.path.join(app.root_path, 'static'),
+		'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/")
+def mainpage():
+	return showStatisticsMenu()
 
 @app.route("/statistics")
 def showStatisticsMenu():
